9f0b7df845c626b9548db459fd8610e5589ba276
Nikola Mihaylov <nikola.mihaylov@luckbox.com>
2020-03-03 17:25:01 +0200

    Trigger build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 module.exports = new Promise((resolve) => {
   const server = app.listen(port, () => {
